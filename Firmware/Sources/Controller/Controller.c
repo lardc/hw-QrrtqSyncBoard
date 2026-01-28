81d8ab663700190d75cabca3821043245d1e2654
IgnatevA1 <a.ignatyev@proton-electrotex.com>
2026-01-28 19:00:33 +0300

    Добавление условия для выбора задержки только в измерении tq

--- Firmware/Sources/Controller/Controller.c
+++ Firmware/Sources/Controller/Controller.c
@@ -358,7 +358,7 @@ void CONTROL_SubProcessStateMachine()
 			else
 			{
 				CONTROL_PulseToPulsePause = CONTROL_TimeCounter
-						+ (LOGIC_DUTTriggered() ? DataTable[REG_PULSE_TO_PULSE_LONG] : UNIT_PULSE_TO_PULSE_FAST);
+						+ (MeasurementMode == MODE_QRR_TQ && LOGIC_DUTTriggered() ? DataTable[REG_PULSE_TO_PULSE_LONG] : UNIT_PULSE_TO_PULSE_FAST);
 				
 				LOGIC_StateRealTime = LSRT_WaitForConfig;
 				LOGIC_ConfigurePrepare();
